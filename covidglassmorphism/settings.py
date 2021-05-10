6c3d71cd684416ed6e1f393c59625551cddbe070
Prasad9404 <mayurshamlaljain@gmail.com>
2021-05-10 11:36:45 +0530

    First commit

--- covidglassmorphism/settings.py
+++ covidglassmorphism/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't%x*tf3)o8pk-9^m+iexl*cupah45h!470i#1d(w4iabu0piff'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','covidprophet.herokuapp.com']
 
 
 # Application definition

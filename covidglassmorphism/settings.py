7905313d5959962d4e26bfcb2ab7306ed8462edc
Prasad9404 <mayurshamlaljain@gmail.com>
2021-05-12 14:59:23 +0530

    final

--- covidglassmorphism/settings.py
+++ covidglassmorphism/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't%x*tf3)o8pk-9^m+iexl*cupah45h!470i#1d(w4iabu0piff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['covidpred.netlify.app','https://covidpred.netlify.app','*.netlify.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
